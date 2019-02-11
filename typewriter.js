ffc7ac2676eac4c6f852256c4872f4daa904e7ac
rebe4175 <36401473+rebe4175@users.noreply.github.com>
2019-02-11 15:29:55 +0100

    Add files via upload

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,42 @@
 "use strict";
+
+window.addEventListener("DOMContentLoaded", init);
+
+let text;
+let counter;
+let slicedText;
+let textL;
+let spaceSound = new Audio("typespace.mp3");
+let audio1 = new Audio("typekey1.mp3");
+let audio2 = new Audio("typekey2.mp3");
+let audioDecider;
+
+function init() {
+  
+    text = document.getElementById("typewriter").textContent;
+    console.log(text);
+    counter = 0;
+    textL = text.length;
+    document.getElementById("typewriter").textContent = "";
+
+   loop();
+}
+
+function loop() {
+
+    slicedText = text.slice(0, counter +1);    
+    audioDecider = slicedText.slice(counter);
+    counter++;
+
+    if (audioDecider == " ") {
+       spaceSound.play();
+    } else {
+       audio1.play();
+    }
+
+    document.getElementById("typewriter").textContent = slicedText;
+
+    if (counter != textL) {
+        setTimeout(loop, 400);
+    } 
+}
